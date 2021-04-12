192a29b54d0526c1071467c3cc126544c47c8930
kfmatovic716 <jabuka611@gmail.com>
2021-04-11 21:49:07 -0700

    Update app.py and on going work with index,html and scrape_mars.py

--- app/app.py
+++ app/app.py
@@ -1,16 +1,29 @@
-# import Flask, pymongo, and scrape_mars (your python file)
+from flask import Flask, render_template, jsonify, redirect
+import pymongo
+from flask_pymongo import PyMongo
+import scrape_mars
 
 # Instantiate a Flask app
-
+app = Flask(__name__)
 
 # Use flask_pymongo to set up mongo connection
+app.config["MONGO_URI"] = "mongodb://localhost:27017/marsmission_app"
+mongo = PyMongo(app)
 
 # Create a base '/' route that will query your mongodb database and render the `index.html` template
+@app.route("/")
+def index():
+    mars = mongo.db.mars.find_one()
+    return render_template("index.html", mars=mars)
+
+# '/scrape' route that will create the mars collection, run scrape() function from scrape_mars, and update the mars collection in the database
+# The route redirects back to the base route '/' with a code 302.
+@app.route("/scrape")
+def scrape():
+    mars_data = scrape_mars.scrape()
+    mongo.db.mars.update({}, mars_data, upsert = True)
+    return redirect("/", code=302)
 
 
-# Create a '/scrape' route that will create the mars collection, run your scrape() function from scrape_mars, and update the mars collection in the database
-# The route should redirect back to the base route '/' with a code 302.
-
-
-
-# Run your app
+if __name__ == "__main__":
+    app.run(debug=True)
--- app/scrape_mars.py
+++ app/scrape_mars.py
@@ -1,5 +1,25 @@
-# Define a function called `scrape` that will execute all of your scraping code from the `mission_to_mars.ipynb` notebook and return one Python dictionary containing all of the scraped data. 
+from splinter import Browser
+from bs4 import BeautifulSoup
+from webdriver_manager.chrome import ChromeDriverManager
+import pandas as pd
+import pymongo
+import requests
+import pathlib
+import pprint
+import time
 
+# Define a function called `scrape` that will execute all of your scraping code from the `mission_to_mars.ipynb` notebook and return one Python dictionary containing all of the scraped data. 
+def scrape():
+
+    # Set the executable path and initialize the chrome browser in splinter
+    executable_path = {'executable_path': ChromeDriverManager().install()}
+    browser = Browser('chrome', **executable_path, headless=True)
+
+    # Visit the mars nasa news site
+    url_nasa = 'https://mars.nasa.gov/news/'
+
+    # Optional delay for loading the page
+    browser.visit(url_nasa)
 # It will be a good idea to create multiple smaller functions that are called by the `scrape()` function. 
 # Remember, each function should have one 'job' (eg. you might have a `mars_news()` function that scrapes the NASA mars news site and returns the content as a list/tuple/dictionary/json)
 # HINT: the headers in the notebook can serve as a useful guide to where one 'job' ends and another begins. 
