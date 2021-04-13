29612ad201e712406cdf2c4fe5b1caede92d03c3
kfmatovic716 <jabuka611@gmail.com>
2021-04-12 23:33:51 -0700

    Update Readme and app.py

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# WebScraping-Mission-to-Mars
+# Mission-to-Mars - A Web Scraping Adventure
+<img src="mission_to_mars.png"/>
+
+## Objectives:
+<ol>
+    <li>To extract data via Web Scraping using Jupyter notebook, and Pandas, BeautifulSoup, & Splinter/Requests python libraries </li>
+    <li>To use MongoDB with Flask templating (Jinja) to create a new HTML page that displays all the scraped information </li>
+</ol>
+
+## Web Scraping 
+<ul>
+    <li>The following sites were visited for Web Scraping:</li>
+        <ul>
+            <li><a href="https://mars.nasa.gov/news/">NASA Mars News Site</a>- latest news title and paragraph text</li>
+            <li><a href="https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/index.html">JPL Mars Space Images</a>- Mars featured image in space</li>
+            <li><a href="https://space-facts.com/mars/">Space Facts Site</a>- Mars facts table</li>
+            <li><a href="https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars">USGS Astrogeology Site</a>- high resolution images of hemispeheres in Mars</li>
+        </ul>
+    <li>scrape_mars.py - Jupyter notebook converted to a Python script that contains the "scrape" function that will execute all of your scraping code from above and returns one Python dictionary containing all of scraped data</li>
+</ul>
+
+## Mongo DB and Flask Application
+<ul>
+    <li>app.py - is a Flask application that will import the `scrape` function from `scrape_mars`. The following routes are available in this app</li>
+        <ul>
+            <li>a root route `/` that will query the Mongo database and pass the mars data into an HTML template to display the data</li>
+            <li>`/scrape` route that imports scrape_mars.py and calls the `scrape` function then stores the return value as a Python dictionary</li>
+        </ul>
+    <li>index.html - an html file using Jinja templating that will take the mars data dictionary and display all of the data in the appropriate HTML elements</li>
+</ul>
--- app/scrape_mars.py
+++ app/scrape_mars.py
@@ -8,7 +8,8 @@ import pathlib
 import pprint
 import time
 
-# Define a function called `scrape` that will execute all of your scraping code from the `mission_to_mars.ipynb` notebook and return one Python dictionary containing all of the scraped data. 
+# Define a function called `scrape` that will execute all of your scraping code from the `mission_to_mars.ipynb` notebook
+# Return one Python dictionary containing all of the scraped data. 
 def scrape():
 
     # Set the executable path and initialize the chrome browser in splinter
@@ -23,3 +24,83 @@ def scrape():
 # It will be a good idea to create multiple smaller functions that are called by the `scrape()` function. 
 # Remember, each function should have one 'job' (eg. you might have a `mars_news()` function that scrapes the NASA mars news site and returns the content as a list/tuple/dictionary/json)
 # HINT: the headers in the notebook can serve as a useful guide to where one 'job' ends and another begins. 
+
+    def init_browser():
+    executable_path = {"executable_path" : "chromedriver"}
+    return Browser("chrome", **executable_path, headless = False)
+
+def scrape():
+    browser = init_browser()
+    data = {}
+    output = mars_news(browser)
+    data['news_title'] = output[0]
+    data['news_paragraph'] = output[1]
+    data['mars_img'] = mars_img(browser)
+    data['mars_weather'] = mars_weather(browser)
+    data['mars_facts'] = mars_fact(browser)
+    data['mars_hemisphere'] = mars_hemisphere(browser)
+    return data
+
+def mars_news(browser):
+    url = "https://mars.nasa.gov/news/"
+    browser.visit(url)
+    
+    time.sleep(10)
+ 
+    html = browser.html
+    soup = bs(html, 'html.parser')
+    article = soup.find('div', class_="list_text")
+    news_title = article.find('div', class_="content_title").text
+    news_p = article.find('div', class_= "article_teaser_body").text
+    output = [news_title, news_p]
+    return output
+
+def mars_img(browser):
+    url_mars_img = "https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars"
+    browser.visit(url_mars_img)
+
+    time.sleep(10)
+    # Find and click the full image button
+
+    html = browser.html
+    soup = bs(html, "html.parser")
+    img = soup.find('img', class_="thumb")['src']
+    img_url = "https://www.jpl.nasa.gov/" +img
+    return img_url
+
+def mars_weather(browser):
+    weather_url ="https://twitter.com/marswxreport?lang=en"
+    browser.visit(weather_url)
+    if browser.is_text_present('InSight sol', wait_time=10):
+
+        html = browser.html
+        soup = bs(html, "html.parser")
+        a = soup.find('div', class_="css-901oao r-hkyrab r-1qd0xha r-a023e6 r-16dba41 r-ad9z0x r-bcqeeo r-bnwqim r-qvutc0")
+        mars_weather = a.find('span', class_="css-901oao css-16my406 r-1qd0xha r-ad9z0x r-bcqeeo r-qvutc0").text.replace("\n", "")
+    return mars_weather
+
+def mars_fact(browser):
+    import pandas as pd
+    mars_facts_url = "https://space-facts.com/mars/"
+    mars_df = pd.read_html(mars_facts_url)[0]
+    mars_html = mars_df.to_html(header = False, index = False)
+    return mars_html
+
+def mars_hemisphere(browser):
+    hemisphere_image_urls=[]
+    mars_hem_url = "https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars"
+    
+    time.sleep(10)
+
+    browser.visit(mars_hem_url)
+    html = browser.html
+    soup = bs(html, "html.parser")
+    list = soup.find_all('div', class_="item")
+    for a in list:
+        title = a.find('h3').text
+        hem_url = "https://astrogeology.usgs.gov/" + a.find("a")["href"]
+        browser.visit(hem_url)
+        soup = bs(browser.html, 'html.parser')
+        img_url = soup.find('li').find('a')['href']
+        hemisphere_image_urls.append({'title': title, 'img_url': img_url})
+    return hemisphere_image_urls
